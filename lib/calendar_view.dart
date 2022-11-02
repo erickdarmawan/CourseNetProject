b7262f067fe101bfd6bfff092695535e44bf0b9b
erickdarmawan <erickdarmawan83@gmail.com>
2022-11-02 16:58:43 +0700

    fixed catboys.dart & arranging calendar_view.dart (unfinished)

--- lib/calendar_view.dart
+++ lib/calendar_view.dart
@@ -1,35 +1,275 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   runApp(const CalendarView());
 }
 
-class CalendarView extends StatelessWidget {
+class CalendarView extends StatefulWidget {
   const CalendarView({Key? key}) : super(key: key);
+
+  @override
+  State<CalendarView> createState() => _CalendarViewState();
+}
+
+class _CalendarViewState extends State<CalendarView> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+
+    loadPreviousEvents() {
+      mySelectedEvents = {
+        '2022-11-22': [
+          {"eventDescp": '11', 'eventTitle': '111'},
+          {"eventDescp": '22', 'eventTitle': '22'}
+        ],
+        '2022-11-30': [
+          {'eventDescp': '22', 'evenTitle': '22'}
+        ]
+      };
+    }
+
+    ;
+  }
+
+  List _listOfDayEvents(DateTime dateTime) {
+    if (mySelectedEvents[DateFormat('yyyy-MM-dd').format(dateTime)] != null) {
+      return mySelectedEvents[DateFormat('yyyy-MM-dd').format(dateTime)]!;
+    } else {
+      return [];
+    }
+  }
+
+  Map<String, List> mySelectedEvents = {};
+  final titleController = TextEditingController();
+  final descpController = TextEditingController();
+
+  _showAddEventDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text(
+                'Add New Event',
+                textAlign: TextAlign.center,
+              ),
+              content: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: titleController,
+                    textCapitalization: TextCapitalization.words,
+                    decoration: const InputDecoration(
+                      labelText: 'Title',
+                    ),
+                  ),
+                  TextField(
+                    controller: descpController,
+                    textCapitalization: TextCapitalization.words,
+                    decoration: const InputDecoration(labelText: 'Description'),
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Cancel')),
+                TextButton(
+                    child: const Text('Add Event'),
+                    onPressed: () {
+                      if (titleController.text.isEmpty &&
+                          descpController.text.isEmpty) {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text('Requiered title and description'),
+                          duration: Duration(seconds: 2),
+                        ));
+                        return;
+                      } else {
+                        print(titleController.text);
+                        print(descpController.text);
+
+                        if (mySelectedEvents[DateFormat('yyyy-MM-dd')
+                                .format(_selectedDay!)] !=
+                            null) {
+                          mySelectedEvents[DateFormat('yyyy-MM-dd')
+                                  .format(_selectedDay!)]
+                              ?.add({
+                            'eventTitle': titleController.text,
+                            'eventDescp': descpController.text,
+                          });
+                        } else {
+                          mySelectedEvents[DateFormat('yyyy-MM-dd')
+                              .format(_selectedDay!)] = [
+                            {
+                              'eventTitle': titleController.text,
+                              'eventDescp': descpController.text
+                            }
+                          ];
+                        }
+
+                        print(
+                            'New event for backend developer ${json.encode(mySelectedEvents)}');
+                        titleController.clear();
+                        descpController.clear();
+                        Navigator.pop(context);
+                        return;
+                      }
+                    }),
+              ],
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
     CalendarFormat format = CalendarFormat.month;
+
+    TextStyle textstyle = TextStyle(
+      color: Colors.black
+    );
+    if(DateTime.sunday > DateTime.saturday)
+    {
+    textstyle = TextStyle(color: Colors.red);
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calendar'),
+        title: const Text('Calendar'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: TableCalendar(
-          focusedDay: DateTime.now(),
-          firstDay: DateTime(2022),
-          lastDay: DateTime(2050),
-          calendarFormat: format,
-          startingDayOfWeek: StartingDayOfWeek.sunday,
-          onFormatChanged: (CalendarFormat _format) {
-            setState() {
-              format = _format;
-            }
-          },
-        ),
+      body: Column(
+        children: [
+          TableCalendar(
+            focusedDay: _focusedDay,
+            firstDay: DateTime(2022),
+            lastDay: DateTime(2050),
+            calendarFormat: _calendarFormat,
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            rowHeight: 60,
+            daysOfWeekHeight: 60,
+            daysOfWeekStyle: DaysOfWeekStyle(
+                weekdayStyle: TextStyle(fontWeight: FontWeight.bold),
+                weekendStyle: TextStyle(color: Colors.red)
+                    ),
+            headerStyle: HeaderStyle(
+                formatButtonVisible: true,
+                titleCentered: true,
+                formatButtonShowsNext: true,
+                formatButtonDecoration: BoxDecoration(
+                    color: Colors.blue.shade200,
+                    borderRadius: BorderRadius.circular(20.0)),
+                formatButtonTextStyle: const TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.bold),
+                titleTextStyle:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                headerPadding:
+                    const EdgeInsets.symmetric(horizontal: 1.0, vertical: 10)),
+            startingDayOfWeek: StartingDayOfWeek.sunday,
+            daysOfWeekVisible: true,
+            calendarStyle: CalendarStyle(
+              isTodayHighlighted: true,
+              
+              weekendTextStyle: TextStyle(color: Colors.red),
+              selectedTextStyle: const TextStyle(color: Colors.white),
+              todayDecoration: BoxDecoration(
+                  color: Colors.purpleAccent,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10.0)),
+              selectedDecoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10.0)),
+              weekendDecoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10)),
+              defaultDecoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10)),
+              tableBorder: TableBorder(
+                  top: BorderSide(
+                    width: 1,
+                    color: Colors.grey.shade300,
+                  ),
+                  bottom: BorderSide(
+                    width: 1,
+                    color: Colors.grey.shade300,
+                  ),
+                  verticalInside: BorderSide(
+                    color: Colors.grey.shade300,
+                    width: 1,
+                  )),
+            ),
+            onDaySelected: (selectedDay, focusedDay) {
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              }
+            },
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+            eventLoader: _listOfDayEvents,
+          ),
+          ..._listOfDayEvents(_selectedDay!).map(
+            (myEvents) => ListTile(
+              leading: Icon(
+                Icons.done,
+                color: Colors.teal,
+              ),
+              title: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Event Title:   ${myEvents['eventTitle']}'),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Description:    ${myEvents['eventDescp']}'),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.only(bottom: 2),
+            decoration: const BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(
+              width: 2,
+            ))),
+            child: const Text(
+              'Event',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                letterSpacing: 2.0,
+                fontSize: 20,
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => _showAddEventDialog(),
+        label: const Text('Add Event'),
       ),
     );
   }
--- lib/catboys.dart
+++ lib/catboys.dart
@@ -1,10 +1,8 @@
-// import 'dart:js';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:photo_view/photo_view.dart';
-import 'package:photo_view/photo_view_gallery.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 class CatboysWidget extends StatefulWidget {
   const CatboysWidget({Key? key}) : super(key: key);
@@ -14,52 +12,107 @@ class CatboysWidget extends StatefulWidget {
 }
 
 class _CatboysWidgetState extends State<CatboysWidget> {
- List<dynamic> imageUrls = [];
+  List<dynamic> imageUrls = [];
+  var numList = [];
+
+  final customCacheManager = CacheManager(
+    Config(
+      'customCacheKey',
+      stalePeriod: const Duration(days: 7),
+      maxNrOfCacheObjects: 20,
+    ),
+  );
+
+  @override
+  void initState() {
+    for (var i = 0; i < 20; i++) {
+      setState(() {
+        numList.add(
+          Text(
+            '${i + 1}',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        );
+      });
+      callCatBoysFromNetwork();
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    for(var i = 0 ; i <= 5; i++){
-      getCatBoys();
-    }
     return Scaffold(
         appBar: AppBar(
           title: const Text('Cat Boys'),
           centerTitle: true,
         ),
-        body:
-         ListView.separated(
-            separatorBuilder: ((context, index) => SizedBox(
-                  height: 12,
-                )),
-            itemCount: imageUrls.length,
-            itemBuilder: (context, index) => ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: CachedNetworkImage(
-                imageUrl: imageUrls[index],
-                key: UniqueKey(),
-                height: 300,
-                width: double.infinity,
-                fit: BoxFit.cover,
-                placeholder: (context, url) => Container(
-                  color: Colors.grey,
-                ),
-                errorWidget: (context, url, error) =>
-                    Center(child: Icon(Icons.error)),
+        body: ListView.separated(
+          separatorBuilder: ((context, index) => const SizedBox(
+                height: 5,
+              )),
+          itemCount: imageUrls.length,
+          itemBuilder: (context, index) => ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Flexible(
+              child: Column(
+                children: [
+                  Center(
+                    child: Container(
+                      color: Colors.grey.shade200,
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              numList[index],
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Flexible(
+                                child: CachedNetworkImage(
+                                  cacheManager: customCacheManager,
+                                  imageUrl: imageUrls[index],
+                                  key: UniqueKey(),
+                                  height: 300,
+                                  width: 520,
+                                  fit: BoxFit.cover,
+                                  placeholder: (context, url) => const Center(
+                                      child: CircularProgressIndicator()),
+                                  errorWidget: (context, url, error) =>
+                                      const Center(child: Icon(Icons.error)),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Divider(
+                    thickness: 1,
+                    color: Colors.black,
+                  ),
+                ],
               ),
             ),
-          )
-    );
+          ),
+        ));
   }
-   Future getCatBoys() async {
-     var url = Uri.https('api.catboys.com', 'img');
-     var response = await http.get(url);
 
-     if (response.statusCode == 200) {
-       var detail = jsonDecode(response.body);
-       String catsImage = detail['url'];
-       setState(() {
-         imageUrls.add(catsImage);
-       });
-     }
-   }
+  Future callCatBoysFromNetwork() async {
+    var url = Uri.https('api.catboys.com', 'img');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var detail = jsonDecode(response.body);
+      String catsImage = detail['url'];
+      setState(() {
+        imageUrls.add(catsImage);
+      });
+    }
+  }
 }
